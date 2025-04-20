bb9153dc91eaa420bc4a0fcdc97848ec92e18e52
Quinn Davis <Quoncdavis@gmail.com>
2025-04-20 14:45:08 -0600

    add simple db tools to dunder init refactor config to allow for use of env variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,32 @@
+from .config import Config
 from .views import app_blueprint
-import flask
+from flask import Flask, g
+import mysql.connector
+
+def get_db():
+    if 'db' not in g:
+        g.db = mysql.connector.connect(
+            host=Config.MYSQL_HOST,
+            user=Config.MYSQL_USER,
+            password=Config.MYSQL_PASSWORD,
+            database=Config.MYSQL_DB
+        )
+        g.cursor = g.db.cursor(dictionary=True)
+    return g.db, g.cursor
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    cursor = g.pop('cursor', None)
+    
+    if cursor is not None:
+        cursor.close()
+    
+    if db is not None:
+        db.close()
 
 def create_app():
-    app = flask.Flask(__name__)
-    app.config.from_pyfile("config.py")
+    app = Flask(__name__)
+    app.config.from_object(Config)
     app.register_blueprint(app_blueprint)
+    app.teardown_appcontext(close_db)
     return app
--- app/config.py
+++ app/config.py
@@ -1,4 +1,12 @@
+from dotenv import load_dotenv
+import os
+
 class Config:
-    SECRET_KEY = "dev"
-    DATABASE="../instance"
+    SECRET_KEY = os.getenv("SECRET_KEY")
     DEBUG=True
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_PORT  = os.getenv("MYSQL_PORT")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+    
